5150ce9077ad7c1658b09b03780321c4c2e145e3
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-08 11:20:34 +0300

    The game works and stop when correct. Hooray!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,27 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number: ");
-    println!("Hit me: ");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read the number");
-    println!("You guessed: {}",guess);
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    loop{
+        let mut guess = String::new();
+        println!("Guess the number: ");
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read the numbers");
+        println!("You guessed: {}",guess);
+        let guess:u32 = guess.trim().parse()
+            .expect("Please type a number!");
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+        }
+
+    }
 }
