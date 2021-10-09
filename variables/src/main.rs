e90f84f26f7dec151d9474c9c43d3587fffa3533
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-09 05:34:06 +0300

    one moment you exist and the next you are replaced

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+
+fn main() {
+    println!("This is the short story of variable x");
+    let x = 5;
+    println!("X was initially {}",x);
+    let x = x + 1;
+    println!("We added 1 and it became {}",x);
+    let x = x * 2;
+    println!("We multiplied by 2 to become {}",x);
+    println!("What happened next will surprise you!");
+    let x = "I am a string";
+    println!("x decided to change its type: x said {}",x);
+}
