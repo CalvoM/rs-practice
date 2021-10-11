77d58e76a00da2fa72c61869560d01eaab1c4c1f
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-11 05:33:22 +0300

    scalar types and tuples

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,4 +10,12 @@ fn main() {
     println!("What happened next will surprise you!");
     let x = "I am a string";
     println!("x decided to change its type: x said {}",x);
+    let number_one = 56u8;
+    println!("number one is {}",number_one);
+    let long_num = 45_111;
+    println!("long num is {}",long_num);
+    let yen = '\u{00a5}';
+    println!("Wanna see the Yen sign, oh I mean {}",yen);
+    let coords: (f32, f32, f32) = (0.0,0.0,0.0);
+    println!("The coordinates for origin is {} {} {}",coords.0,coords.1,coords.2);
 }
