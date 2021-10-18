a910ffa0a48dc947ba6348168bbb8d86ec0d62a6
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-18 12:31:04 +0300

    variable moving in functions args

--- ownership_string/src/main.rs
+++ ownership_string/src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
     let s = String::from("hello");
+    takes_ownership(s);
     println!("{}",s);
-    let s1 = s;
-    let s2 = s1.clone();
-    println!("{} {}",s2,s1);
+    let x = 5;
+    makes_copy(x);
+    println!("{}",x);
+}
+
+fn takes_ownership(some_string: String){
+    println!("{}",some_string);
+}
+
+fn makes_copy(some_integer: i32){
+    println!("{}", some_integer);
 }
