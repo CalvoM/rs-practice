73ff8d9044ac2416c1d5e7d74dbc8c00197b34d7
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-27 14:11:22 +0300

    introduce struct methods

--- area/src/main.rs
+++ area/src/main.rs
@@ -3,12 +3,20 @@ struct Rectangle{
     width: u32,
     height: u32,
 }
+
+impl Rectangle{
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+}
+
 fn main() {
     let width = 30;
     let length = 50;
     let rect1 = (30, 50);
     let rect2 = Rectangle{width:30,height:50};
-    println!("{} * {} = {}/{}/{}\r\nType of {:?}\r\n{:#?}",width,length,area(width, length),area_by_tuples(rect1),area_by_struct(&rect2),rect2,rect2)
+    println!("{} * {} = {}/{}/{}\r\nType of {:?}\r\n{:#?}",width,length,area(width, length),area_by_tuples(rect1),area_by_struct(&rect2),rect2,rect2);
+    println!("{}",rect2.area());
 }
 
 fn area(w: u32, l: u32) -> u32{
