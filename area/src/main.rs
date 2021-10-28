e306e11697ade2d91ffa24842922ba92c3712aac
d1r3ct0r <mwadimemakokha@gmail.com>
2021-10-28 16:06:47 +0300

    associate functions

--- area/src/main.rs
+++ area/src/main.rs
@@ -8,25 +8,20 @@ impl Rectangle{
     fn area(&self) -> u32{
         self.width * self.height
     }
+    fn can_fit(&self, other:&Rectangle) -> bool{
+        self.width >= other.width && self.height >= other.height
+    }
+    fn square(size:u32) -> Rectangle{
+        Rectangle{width:size, height:size}
+    }
 }
+
 
 fn main() {
-    let width = 30;
-    let length = 50;
-    let rect1 = (30, 50);
-    let rect2 = Rectangle{width:30,height:50};
-    println!("{} * {} = {}/{}/{}\r\nType of {:?}\r\n{:#?}",width,length,area(width, length),area_by_tuples(rect1),area_by_struct(&rect2),rect2,rect2);
-    println!("{}",rect2.area());
-}
-
-fn area(w: u32, l: u32) -> u32{
-    w * l
-}
-
-fn area_by_tuples(dimensions: (u32,u32)) -> u32{
-    dimensions.0 * dimensions.1
-}
-
-fn area_by_struct(rect: &Rectangle) -> u32{
-    rect.width * rect.height
+    let rect1 = Rectangle{width:30,height:50};
+    let rect2 = Rectangle{width:10,height:10};
+    let square = Rectangle::square(20);
+    println!("The area of the rectangles are {} and {}",rect1.area(),rect2.area());
+    println!("Can the first rectangle hold the second? {}",rect1.can_fit(&rect2));
+    println!("The area of our square is {}",square.area())
 }
