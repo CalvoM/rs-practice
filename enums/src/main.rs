348c4db0ca3d01220afcd5ec60dd56e26925ebe2
d1r3ct0r <mwadimemakokha@gmail.com>
2021-11-09 06:30:32 +0300

    adding methods in enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -2,7 +2,24 @@ enum IpAddr{
     V4(u8,u8,u8,u8),
     V6(String),
 }
+
+
+enum Message{ //equivalent struct as commented below
+    Quit, //Struct QuitMessage;
+    Move {x: i32, y: i32}, //Struct MoveMessage{x: i32, y: i32};
+    Write(String), //Struct WriteMessage(String);
+    ChangeColor(i32, i32, i32), //Struct ChangeColorMessage(i32, i32, i32);
+}
+
+impl Message{
+    fn call(&self){
+        println!("{}", "What");
+    }
+}
+
 fn main() {
-    let home = IpAddr::V4(127,0,0,1);
-    let loopback = IpAddr::V6(String::from("::1"));
+    let _home = IpAddr::V4(127,0,0,1);
+    let _loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("Hello"));
+    println!("{}", m.call());
 }
