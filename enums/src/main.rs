f1190b4f519a6cbdcdb4bf911a25cafe65d843d0
d1r3ct0r <mwadimemakokha@gmail.com>
2021-11-09 06:46:52 +0300

    some option work done

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,25 +1,8 @@
-enum IpAddr{
-    V4(u8,u8,u8,u8),
-    V6(String),
-}
-
-
-enum Message{ //equivalent struct as commented below
-    Quit, //Struct QuitMessage;
-    Move {x: i32, y: i32}, //Struct MoveMessage{x: i32, y: i32};
-    Write(String), //Struct WriteMessage(String);
-    ChangeColor(i32, i32, i32), //Struct ChangeColorMessage(i32, i32, i32);
-}
-
-impl Message{
-    fn call(&self){
-        println!("{}", "What");
-    }
-}
-
 fn main() {
-    let _home = IpAddr::V4(127,0,0,1);
-    let _loopback = IpAddr::V6(String::from("::1"));
-    let m = Message::Write(String::from("Hello"));
-    println!("{}", m.call());
+    let some_number = Some(5);
+    let some_string = Some("a string");
+    let absent_number: Option<i32> = None;
+    let def_number: i8 = 5;
+    let sum = some_number + def_number;
+    println!("{}", sum);
 }
