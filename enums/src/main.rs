650f2867cb41fc0d261422d98fa9ab1880bfcf75
d1r3ct0r <mwadimemakokha@gmail.com>
2021-11-09 07:11:39 +0300

    getting started with if let

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,76 +1,8 @@
-#[derive(Debug)]
-enum State{
-    Alabama,
-    Alaska,
-    Arizona,
-    Arkansas,
-    California,
-    Colorado,
-    Connecticut,
-    Delaware,
-    Florida,
-    Georgia,
-    Hawaii,
-    Idaho,
-    Illinois,
-    Indiana,
-    Iowa,
-    Kansas,
-    Kentucky,
-    Louisiana,
-    Maine,
-    Maryland,
-    Massachusetts,
-    Michigan,
-    Minnesota,
-    Mississippi,
-    Missouri,
-    Montana,
-    Nebraska,
-    Nevada,
-    NewHampshire,
-    NewJersey,
-    NewMexico,
-    NewYork,
-    NorthCarolina,
-    NorthDakota,
-    Ohio,
-    Oklahoma,
-    Oregon,
-    Pennsylvania,
-    RhodeIsland,
-    SouthCarolina,
-    SouthDakota,
-    Tennessee,
-    Texas,
-    Utah,
-    Vermont,
-    Virginia,
-    Washington,
-    WestVirginia,
-    Wisconsin,
-    Wyoming,
-}
-enum Coin{
-    _Penny,
-    _Nickel,
-    _Dime,
-    Quarter(State),
-}
-
-fn get_cents(coin: Coin) -> u32{
-    match coin{
-        Coin::_Penny => 1,
-        Coin::_Nickel => 5,
-        Coin::_Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        },
-    }
-}
-
 fn main() {
-    let my_coin = Coin::Quarter(State::Nebraska);
-    println!("Number of cents for coin = {}", get_cents(my_coin));
+    let some_u8_value = Some(8);
+    if let Some(3) = some_u8_value{
+        println!("three")
+    }else{
+        println!("not three")
+    }
 }
