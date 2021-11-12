e751fa334d75ea4ebf67b722825af14009b6c163
d1r3ct0r <mwadimemakokha@gmail.com>
2021-11-12 05:43:09 +0300

    bit of testing between siblings

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,11 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
-
 pub mod network;
 pub mod client;
+
+#[cfg(test)]
+mod tests {
+    use super::client;
+    #[test]
+    fn it_works() {
+        // let result = 2 + 2;
+        // assert_eq!(result, 4);
+        client::connect();
+    }
+}
