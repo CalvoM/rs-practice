c7820797a84360ec8d11845f89f2acafcc4349fe
d1r3ct0r <mwadimemakokha@gmail.com>
2021-11-12 05:29:01 +0300

    make all public

--- mod_comp/src/lib.rs
+++ mod_comp/src/lib.rs
@@ -1,14 +1,13 @@
 mod outermost{
     pub fn middle_function(){}
-    fn middle_secret_function(){}
-    mod inside{
-        ::outermost::middle_secret_function!();
+    pub fn middle_secret_function(){}
+    pub mod inside{
         pub fn inner_function(){}
         pub fn secret_function(){}
     }
 }
 
-fn try_me(){
+pub fn try_me(){
     outermost::middle_function();
     outermost::middle_secret_function();
     outermost::inside::inner_function();
