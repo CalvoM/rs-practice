88b7de1198789b7377d8fa1e303d1d57bbc16c54
d1r3ct0r <mwadimemakokha@gmail.com>
2021-11-23 06:19:39 +0300

    HashMap operations

---
+++ hashmaps/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut _scores = HashMap::new();
+    _scores.insert(String::from("Blue"), 10);
+    _scores.insert(String::from("Yellow"), 50);
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10,50];
+    let mut scores: HashMap<_,_> = teams.iter().zip(initial_scores.iter()).collect();
+    for s in scores.iter() {
+        println!("{} = {}",s.0,s.1);
+    }
+    for (k,v) in &scores {
+        println!("{} = {} ",k,v);
+    }
+    let blue = String::from("Blue");
+    let sc = 20;
+    scores.insert(&blue,&sc);
+    scores.entry(&blue).or_insert(&60);
+    println!("{:?}", scores);
+    let _blue_score = scores.get(&String::from("Blue"));
+}
