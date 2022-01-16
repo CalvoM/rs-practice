1f31fd8e41e2e459ec7d0596723d0b057d7eba0e
CalvoM <mwadimemakokha@gmail.com>
2022-01-16 06:36:43 +0300

    basic propagation impl

--- traits_basics/src/main.rs
+++ traits_basics/src/main.rs
@@ -1,5 +1,21 @@
 use std::fs::File;
+use std::io::self;
 
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("random1.txt");
+
+    let mut f = match f{
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+
+}
 fn main() {
-    let f = File::open("hello.txt").unwrap();
+    let f = File::open("hello.txt").expect("Failed to open file needed");
 }
