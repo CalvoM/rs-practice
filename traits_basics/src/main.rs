072e292148f8213c295d5f26b0286c4d20ff688d
CalvoM <mwadimemakokha@gmail.com>
2022-01-16 06:23:13 +0300

    handle errors with unwrap

--- traits_basics/src/main.rs
+++ traits_basics/src/main.rs
@@ -1,18 +1,5 @@
 use std::fs::File;
-use std::io::ErrorKind;
 
 fn main() {
-    let f = File::open("random.txt");
-    let f = match f{
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("random.txt") {
-                Ok(fc) => fc, 
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file : {:?}", other_error)
-            }
-        },
-    };
+    let f = File::open("hello.txt").unwrap();
 }
