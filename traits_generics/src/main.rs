4725808db9b9fa6983f7d406ecda4796f00fc443
CalvoM <mwadimemakokha@gmail.com>
2022-01-18 06:22:50 +0300

    solve duplication by refactoring

--- traits_generics/src/main.rs
+++ traits_generics/src/main.rs
@@ -1,18 +1,17 @@
+fn get_largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list {
+        if item > largest {
+            largest = item
+        }
+    }
+    largest
+}
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let mut largest = number_list[0];
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
+    let largest = get_largest(&number_list);
     println!("The largest number is {}", largest);
     let number_list = vec![102, 34, 600, 3, 90, 188];
-    let mut largest = number_list[0];
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
+    let largest = get_largest(&number_list);
     println!("The largest number is {}", largest)
 }
