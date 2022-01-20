d83ecb1856dd71e3b4b38202d236b73587f3025f
CalvoM <mwadimemakokha@gmail.com>
2022-01-20 04:38:52 +0300

    same code in different fxns but diff types

--- traits_generics/src/main.rs
+++ traits_generics/src/main.rs
@@ -1,4 +1,4 @@
-fn get_largest(list: &[i32]) -> i32 {
+fn get_largest_num(list: &[i32]) -> i32 {
     let mut largest = list[0];
     for &item in list {
         if item > largest {
@@ -7,11 +7,21 @@ fn get_largest(list: &[i32]) -> i32 {
     }
     largest
 }
+
+fn get_largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+    for & item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let largest = get_largest(&number_list);
+    let largest = get_largest_num(&number_list);
     println!("The largest number is {}", largest);
-    let number_list = vec![102, 34, 600, 3, 90, 188];
-    let largest = get_largest(&number_list);
-    println!("The largest number is {}", largest)
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = get_largest_char(&char_list);
+    println!("The largest of char is {}", result);
 }
