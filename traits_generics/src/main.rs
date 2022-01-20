4629b8a605cdca59a579f1acc80634ff7e5d9c56
CalvoM <mwadimemakokha@gmail.com>
2022-01-20 04:48:41 +0300

    generic fxn but needs traits to work

--- traits_generics/src/main.rs
+++ traits_generics/src/main.rs
@@ -1,16 +1,6 @@
-fn get_largest_num(list: &[i32]) -> i32 {
+fn get_largest<T>(list: &[T]) -> T{
     let mut largest = list[0];
     for &item in list {
-        if item > largest {
-            largest = item
-        }
-    }
-    largest
-}
-
-fn get_largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-    for & item in list {
         if item > largest {
             largest = item;
         }
@@ -19,9 +9,9 @@ fn get_largest_char(list: &[char]) -> char {
 }
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let largest = get_largest_num(&number_list);
+    let largest = get_largest(&number_list);
     println!("The largest number is {}", largest);
     let char_list = vec!['y', 'm', 'a', 'q'];
-    let result = get_largest_char(&char_list);
+    let result = get_largest(&char_list);
     println!("The largest of char is {}", result);
 }
