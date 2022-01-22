205c1ab5574433b68a99c46b2badaf9a7ea4531e
CalvoM <mwadimemakokha@gmail.com>
2022-01-22 15:17:53 +0300

    mix up of the point values

--- traits_generics/src/main.rs
+++ traits_generics/src/main.rs
@@ -13,6 +13,11 @@ struct Point<T> {
     y: T,
 }
 
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32{
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
 struct InclusivePoint<T, U> {
     x: T,
     y: U,
@@ -25,6 +30,13 @@ impl<T, U> InclusivePoint<T, U> {
     fn y(&self) -> &U {
         &self.y
     }
+    
+    fn mixup<V,W> (self, other: InclusivePoint<V, W>) -> InclusivePoint<T,W> {
+        InclusivePoint{
+            x: self.x,
+            y: other.y,
+        }
+    }
 }
 fn main() {
 //    let number_list = vec![34, 50, 25, 100, 65];
@@ -33,9 +45,13 @@ fn main() {
 //    let char_list = vec!['y', 'm', 'a', 'q'];
 //    let result = get_largest(&char_list);
 //    println!("The largest of char is {}", result);
-    let _integer_point = Point{x: 5, y: 10};
-    let _float_point = Point{x: 1.0, y: 5.0};
+    let float_point = Point{x: 3.0, y: 4.0};
+    println!("Distance for the origin is for floating point is {}", float_point.distance_from_origin());
     let cultured_point = InclusivePoint{x:1, y: 3.0}; //diff types, ok
+    let confused_point = InclusivePoint{x: 5, y: 9.0};
     println!("p.x = {}", cultured_point.x());
     println!("p.y = {}", cultured_point.y());
+    let mixed = cultured_point.mixup(confused_point);
+    println!("mixed x = {}", mixed.x());
+    println!("mixed y = {}", mixed.y());
 }
