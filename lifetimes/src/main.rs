b5ba71b3fcb60278b46a14382683267b3aa2a9e4
CalvoM <mwadimemakokha@gmail.com>
2022-02-04 05:34:49 +0300

    dusting lifetime

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,8 +1,17 @@
-fn main() {
-    let r;
-    {
-        let x = 5;
-        r = &x;
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        let e = x;
+        e
+    } else {
+        y
     }
-    println!("r = {}",r);
+}
+fn main() {
+    let s1 = String::from("abcd");
+    let res;
+    {
+        let s2 = String::from("xyz");
+        res = longest(s1.as_str(), s2.as_str());
+    }
+        println!("The longest string is {}", res);
 }
