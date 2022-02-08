26ae138c3cdf02667653846fc5d790c93446ed21
CalvoM <mwadimemakokha@gmail.com>
2022-02-08 05:35:44 +0300

    tests: basics done

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -6,8 +6,25 @@ struct Rectangle {
 
 impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width < other.width && self.height > other.height
+        self.width > other.width && self.height > other.height
     }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn panicky_work(a: i32) -> i32 {
+    if a > 100 && a <200 {
+        panic!("You are overestimating our abilities");
+    } else if a > 200 {
+        panic!("Wow!");
+    }
+    a
+}
+
+pub fn greeting(name: &str) -> String {
+    String::from("Hello!")
 }
 
 #[cfg(test)]
@@ -41,4 +58,23 @@ mod tests {
         assert!(!smaller.can_hold(&larger));
     }
 
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+        !result.contains("Carol"),
+        "Greeting did not contain name, value was `{}`",
+        result
+        );
+    }
+    #[test]
+    #[should_panic(expected = "You are overestimating our abilities" )]
+    fn greater_than_100(){
+        panicky_work(401);
+    }
 }
