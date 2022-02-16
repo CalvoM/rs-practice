1e88a72671d20bbfb7059cc44af7a3b00d209dcd
CalvoM <mwadimemakokha@gmail.com>
2022-02-16 05:44:01 +0300

    basic struct and function

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,17 +3,22 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    let config = parse_config(&args);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
     println!("Reading from mock file to test functionality");
     let filename = "./poem.txt";
     let contents = fs::read_to_string(filename).expect("Error while reading file");
     println!("{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config{
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config{ query, filename }
 }
