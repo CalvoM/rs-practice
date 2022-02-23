371ef6ef917faf4ded4d0c79bd2583605ddd5854
CalvoM <mwadimemakokha@gmail.com>
2022-02-23 04:52:10 +0300

    minigrep: split concerns in different files

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,31 +1,18 @@
 use std::env;
-use std::fs;
-
+use std::process;
+use minigrep::Config;
+use minigrep::run;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-    println!("Reading from mock file to test functionality");
-    let filename = "./poem.txt";
-    let contents = fs::read_to_string(filename).expect("Error while reading file");
-    println!("{}", contents);
-}
-
-struct Config{
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config{ query, filename })
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments : {}", err);
+        process::exit(1);
+    });
+    println!("[ Searching for {} ]", config.query);
+    println!("[ In file {} ]", config.filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
-
 }
 
