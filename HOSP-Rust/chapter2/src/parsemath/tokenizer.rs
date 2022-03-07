569484442c76f893eb284bd05cd263c83ecbcd59
CalvoM <mwadimemakokha@gmail.com>
2022-03-07 05:01:08 +0300

    complete number tokenizing

--- HOSP-Rust/chapter2/src/parsemath/tokenizer.rs
+++ HOSP-Rust/chapter2/src/parsemath/tokenizer.rs
@@ -17,7 +17,19 @@ impl<'a> Tokenizer<'a> {
     fn next(&mut self) -> Option<Token> {
         let next_char = self.expr.next();
         match next_char {
-            Some('0'..='9') => {},
+            Some('0'..='9') => {
+                let mut number = next_char?.to_string();
+                while let Some(next_char) = self.expr.peek() {
+                    if next_char.is_numeric() || next_char == &'.' {
+                        number.push(self.expr.next()?);
+                    } else if next_char == &'(' {
+                        return None;
+                    } else {
+                        break;
+                    }
+                }
+                Some(Token::Num(number.parse::<f64>().unwrap()))
+            },
             Some('+') => Some(Token::Add),
             Some('-') => Some(Token::Subract),
             Some('*') => Some(Token::Multiply),
