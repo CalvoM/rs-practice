452f9318f1de5831b4db8ad1f2cbb8541fdcd28f
CalvoM <mwadimemakokha@gmail.com>
2022-04-02 06:16:09 +0300

    fibonacci_series: done

--- random_projects/src/main.rs
+++ random_projects/src/main.rs
@@ -1,4 +1,9 @@
-use practice::q6::query::guessing_game;
+use practice::q8::query::print_fibonacci;
+use practice::commons::get_user_input_uint;
+use std::io::Write;
 fn main(){
-    guessing_game();
+    print!("Provide limit for the fibonacci series: ");
+    std::io::stdout().flush().unwrap();
+    let limit = get_user_input_uint();
+    print_fibonacci(0,1,limit);
 }
