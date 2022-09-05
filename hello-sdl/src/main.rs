cdaa6fbe4a76a14d627c699ae3ed877bf71b1ac5
CalvoM <mwadimemakokha@gmail.com>
2022-09-05 21:45:51 +0300

    sdl: begin building

--- hello-sdl/src/main.rs
+++ hello-sdl/src/main.rs
@@ -1,4 +1,9 @@
-use sdl2::{pixels::Color, image::{InitFlag, LoadTexture}};
+use sdl2::{
+    event::Event,
+    image::{InitFlag, LoadTexture},
+    keyboard::Keycode,
+    pixels::Color,
+};
 use std::time::Duration;
 fn main() {
     let sdl_ctx = sdl2::init().unwrap();
@@ -7,8 +12,26 @@ fn main() {
     let window = video.window("Demo", 800, 600).opengl().build().unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
     let texture_creator = canvas.texture_creator();
-    let texture = texture_creator.load_texture("./resources/hello_world.bmp").unwrap();
+    let texture = texture_creator
+        .load_texture("./resources/hello_world.bmp")
+        .unwrap();
     canvas.copy(&texture, None, None);
     canvas.present();
-    ::std::thread::sleep(Duration::new(5,0));
+    'mainloop: loop {
+        for event in sdl_ctx.event_pump().unwrap().poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'mainloop,
+                Event::KeyDown {
+                    keycode: Some(Keycode::A),
+                    ..
+                } => break 'mainloop,
+                Event::MouseMotion { ..} => {
+                    let mut mouse_state = sdl_ctx.event_pump().unwrap().mouse_state();
+                    println!("Mouse coords: x = {} y = {}", mouse_state.x(), mouse_state.y());
+                }
+                _ => {}
+            }
+        }
+        ::std::thread::sleep(Duration::new(0, 1));
+    }
 }
