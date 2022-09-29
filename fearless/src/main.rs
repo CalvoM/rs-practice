e4c942d9098cb64e59bd6376edb808da930f5298
CalvoM <mwadimemakokha@gmail.com>
2022-09-29 20:11:15 +0300

    some threading

--- fearless/src/main.rs
+++ fearless/src/main.rs
@@ -1,21 +1,19 @@
 use std::thread;
-use std::sync::{mpsc, Mutex};
+use std::sync::{mpsc, Mutex, Arc};
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let numbers = vec![1,2,3,4,5,6,7,8,9];
-    let m = Mutex::new(5);
-    thread::spawn(move || {
-        for n in numbers {
-            tx.send(n).unwrap();
-        }
-    });
-    for recv in rx {
-        println!("Received: {}", recv);
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
     }
-    {
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    for handle in handles {
+        handle.join().unwrap();
     }
-    println!("m = {:?}", m);
+    println!("Result:\t{}", *counter.lock().unwrap());
 }
