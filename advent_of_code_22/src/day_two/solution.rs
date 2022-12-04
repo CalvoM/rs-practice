11ab480243b437dd09cd7de5bacf10e3f8c6c6b0
CalvoM <mwadimemakokha@gmail.com>
2022-12-04 21:26:41 +0300

    day_two: quesday_two: question two done

--- advent_of_code_22/src/day_two/solution.rs
+++ advent_of_code_22/src/day_two/solution.rs
@@ -2,10 +2,7 @@ use std::{collections::HashMap, fs};
 pub fn question_1() {
     let ops_values: HashMap<&str, u32> = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
     let my_values: HashMap<&str, u32> = HashMap::from([("X", 1), ("Y", 2), ("Z", 3)]);
-    let mut counter_values: HashMap<u32, u32> = HashMap::new();
-    counter_values.insert(1, 3);
-    counter_values.insert(2, 1);
-    counter_values.insert(3, 2);
+    let mut counter_values: HashMap<u32, u32> = HashMap::from([(1, 3), (2, 1), (3, 2)]);
     let contents = fs::read_to_string("./src/day_two/input.txt").expect("Reading not okay");
     let results = contents.trim().split("\n");
     let mut total_score: u64 = 0;
@@ -22,5 +19,42 @@ pub fn question_1() {
             total_score += *me as u64;
         }
     }
+    println!("total score = {}", total_score);
+}
+pub fn question_2() {
+    #[derive(PartialEq, Eq, Debug)]
+    enum WinState {
+        LOSE,
+        DRAW,
+        WIN,
+    }
+    let ops_values: HashMap<&str, u32> = HashMap::from([("A", 1), ("B", 2), ("C", 3)]);
+    let expected_win_values: HashMap<&str, WinState> = HashMap::from([
+        ("X", WinState::LOSE),
+        ("Y", WinState::DRAW),
+        ("Z", WinState::WIN),
+    ]);
+    let mut win_values: HashMap<u32, u32> = HashMap::from([(1, 3), (2, 1), (3, 2)]);
+    let mut loss_values: HashMap<u32, u32> = HashMap::from([(1, 2), (2, 3), (3, 1)]);
+    let contents = fs::read_to_string("./src/day_two/input.txt").expect("Reading not okay");
+    let results = contents.trim().split("\n");
+    let mut total_score: u64 = 0;
+    for result in results {
+        let values: Vec<&str> = result.split_whitespace().collect();
+        let opponent = ops_values.get(values[0]).unwrap_or(&0);
+        let win_state = expected_win_values
+            .get(values[1])
+            .unwrap_or(&WinState::LOSE);
+        if win_state == &WinState::DRAW {
+            total_score += 3 + (*opponent as u64);
+        } else if win_state == &WinState::WIN {
+            let shape = loss_values.get(opponent).unwrap_or(&0);
+            total_score += 6 + (*shape as u64);
+        } else {
+            let shape = win_values.get(opponent).unwrap_or(&0);
+            total_score += *shape as u64;
+        }
+    }
+
     println!("total score = {}", total_score);
 }
