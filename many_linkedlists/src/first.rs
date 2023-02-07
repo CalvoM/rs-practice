a3db3330057e2eefbc689b2b075f9c191f63b4b2
CalvoM <mwadimemakokha@gmail.com>
2023-02-07 10:06:28 +0300

    many ll: first layout almost done

---
+++ many_linkedlists/src/first.rs
@@ -0,0 +1,27 @@
+// Memory inefficient linked list due to tail node being junk and still allocated
+// pub enum List {
+//     Empty,
+//     Elem(i32, Box<List>),
+// }
+
+// Even though Empty is not called
+// Does not have null pointer optimization like above approach
+// pub enum List {
+//     Empty,
+//     ElemThenEmpty(i32),
+//     ElemThenNotEmpty(i32, Box<List>),
+// }
+
+// no extra junk at the end, due to npo
+// elements uniformly allocated
+// /////////////////////////////////////////
+// struct Node {
+//     elem: i32,
+//     next: List,
+// }
+//
+// pub enum List {
+//     Empty,
+//     More(Box<Node>),
+// }
+// /////////////////////////////////////////
