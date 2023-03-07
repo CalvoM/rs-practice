60800fc9a9b7ff256a7dc52aac549a4f24415c19
CalvoM <mwadimemakokha@gmail.com>
2023-03-07 09:38:14 +0300

    remove unnecessary

--- random_projects/src/main.rs
+++
@@ -1,9 +0,0 @@
-use practice::q11::query::word_scrambler;
-use practice::commons::{get_user_input_str, stdout_fflush};
-fn main(){
-    print!("Enter the string to scramble: ");
-    stdout_fflush();
-    let input: String = get_user_input_str().trim().to_string();
-    let c = word_scrambler(&input);
-    println!("{:?}", c);
-}
--- random_projects/src/q1/query.rs
+++
@@ -1,8 +0,0 @@
-
-pub fn factorial(num: u32) -> u32 {
-    if num < 1 {
-        1
-    } else {
-        num * factorial(num -1)
-    }
-}
