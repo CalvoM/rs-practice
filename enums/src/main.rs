834829aafed38ffa1608d2ad85d348fb1f536962
CalvoM <mwadimemakokha@gmail.com>
2023-03-07 09:48:19 +0300

    some linting

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let some_u8_value = Some(8);
-    if let Some(3) = some_u8_value{
+    if let Some(3) = some_u8_value {
         println!("three")
-    }else{
+    } else {
         println!("not three")
     }
 }
