c4d4837e8958c2987f2b5973dd6bdb7067600f03
CalvoM <mwadimemakokha@gmail.com>
2023-03-27 21:59:33 +0300

    tcp adding multithreading

--- tcp_works/src/main.rs
+++ tcp_works/src/main.rs
@@ -1,14 +1,24 @@
-use std::fs;
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
 
 macro_rules! pages {
     ($file: expr) => {{
         format!("pages/{}", $file)
     }};
 }
+
+pub struct ThreadPool;
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        ThreadPool
+    }
+}
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
@@ -18,21 +28,16 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let mut status_line: &str;
-    let mut contents: String = String::new();
-    if request_line == "GET / HTTP/1.1" {
-        status_line = "HTTP/1.1 200 OK";
-        contents = fs::read_to_string(pages!("index.html")).unwrap();
-    } else {
-        status_line = "HTTP/1.1 404 NOT FOUND";
-        contents = fs::read_to_string(pages!("404.html")).unwrap();
-    }
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", pages!("index.html")),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", pages!("index.html"))
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", pages!("404.html")),
+    };
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
